53ab68a84beda5b06c683392fbe63205d5c4eebe
Kristine <chankristineagcay@gmail.com>
2022-11-12 13:28:29 +0800

    implemented option 1

--- dictionary.py
+++ dictionary.py
@@ -1,22 +1,38 @@
 # Write a python program for contact tracing:
 # Display a menu
-print()
+print('\n \n')
 print("                                      ===================== CONTACT TRACING MENU ========================")
 print()
-print("                                                             [1] Add an Item")
-print("                                                             [2] Search an Item")
-print("                                                             [3] Exit Program")
+print("                                                         [1] Add and Informantions")
+print("                                                         [2] Search an Informations")
+print("                                                         [3] Exit Program")
 print()
 print("                                      ===================================================================")
 
 
 # Ask User for input 
-print()
-print()
-user_in = int(input("Annyeonghaseyo, Dear User! What do you want to do?  "))
+print('\n \n')
+user_in = int(input("                                          Annyeonghaseyo, Dear User! What do you want to do? [1-3] : "))
+print('\n \n')
 
-# Option 1
 
+info= {}
+
+# Option 1 - Add an Item 
+if user_in == 1:
+    add_in= int(input("                                                    How Many Infos do you want to Add?: "))  
+    for i in range (add_in):
+        key=input("                                                        What do you want to add: ")
+        value=input("                                                      Enter your Information: ")
+        info[key]= value
+        print ('\n')
+        print ("                                                       Your Information has been saved!")
+        print ('\n')
+        print(info)
+        print('\n')
+        print("==========================================================================================================================================")
+        print ('\n \n')
+    
 # Option 2
 # Option 3
 
