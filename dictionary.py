2352997733d456d672df39fab7b79c0670129ba6
Kristine <chankristineagcay@gmail.com>
2022-11-12 18:36:09 +0800

    edited option 1: Working option 1

--- dictionary.py
+++ dictionary.py
@@ -1,22 +1,47 @@
 # Write a python program for contact tracing:
+
+
+# Dictionary   
+info  = {}
+
 # Display a menu
-print()
+print('\n \n')
 print("                                      ===================== CONTACT TRACING MENU ========================")
 print()
-print("                                                             [1] Add an Item")
-print("                                                             [2] Search an Item")
-print("                                                             [3] Exit Program")
+print("                                                         [1] Add and Informantions")
+print("                                                         [2] Search an Informations")
+print("                                                         [3] Exit Program")
 print()
 print("                                      ===================================================================")
 
 
+
 # Ask User for input 
-print()
-print()
-user_in = int(input("Annyeonghaseyo, Dear User! What do you want to do?  "))
+while True:
+    print('\n \n')
+    user_in = int(input("                                          Annyeonghaseyo, Dear User! What do you want to do? [1-3] : "))
+    print('\n \n')
 
-# Option 1
+    
+# Option 1 - Add an Item from user
+    if user_in == 1:
+        add_in= int(input("                                                    How Many Infos do you want to Add?: "))  
+        u_name= input("Kindly input your Full Name: ")
+        u_age=str(input("Kindly input your Age: "))
+        u_gender= str(input("Kindly input your Gender (F/M): "))
+        u_status=input("Kindly input your Status: ")
+        u_num = str(input("Kindly input your Phone Number:"))
+        u_add = input("Kindly Put your Address: ")
+        u_occu = (input("Kindly input your Occupation:" ))
+        print ('\n')
+        print ("                                                       Your Information has been saved!")
+        print('\n')
+        print("==========================================================================================================================================")
+        print ('\n \n')
 
+        info[u_name]= [u_age,  u_gender, u_status, u_num, u_add, u_occu]
+        new_info = info
+        
+             
 # Option 2
 # Option 3
-
