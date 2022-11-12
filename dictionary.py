88ab1b34f77c551399338f3d311b6863e75829fc
Kristine <chankristineagcay@gmail.com>
2022-11-12 11:38:41 +0800

    implemented Menu options

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# Write a python program for contact tracing:
+# Display a menu
+print("")
+print("                                      ===================== CONTACT TRACING MENU ========================")
+print()
+print("                                                             [1] Add an Item")
+print("                                                             [2] Search an Item")
+print("                                                             [3] Exit Program")
+print("")
+print("                                      ===================================================================")
+# Option 1
+# Option 2
+# Option 3
+
