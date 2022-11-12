317f4a6ed4aa163d076b688e2733f9638898c2bc
Kristine <chankristineagcay@gmail.com>
2022-11-12 20:27:19 +0800

    Working program: Fixed all errors and apperance of  the program

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,9 @@
 # Write a python program for contact tracing:
 
 
-# Dictionary   
-info  = {}
-
 # Display a menu
 print('\n \n')
-print("                                      ===================== CONTACT TRACING MENU ========================")
+print("                                      ===================== \033[01mCONTACT TRACING MENU\33[0m ========================")
 print()
 print("                                                         [1] Add and Informantions")
 print("                                                         [2] Search an Informations")
@@ -14,34 +11,37 @@ print("                                                         [3] Exit Program
 print()
 print("                                      ===================================================================")
 
-
+# Dictionary   
+info  = {}
 
 # Ask User for input 
 while True:
     print('\n \n')
-    user_in = int(input("                                          Annyeonghaseyo, Dear User! What do you want to do? [1-3] : "))
+    user_in = int(input("                                          \033[32mAnnyeonghaseyo, Dear User! What do you want to do? [1-3] :\33[0m "))
     print('\n \n')
 
     
 # Option 1 - Add an Item from user
     if user_in == 1:
-        add_in= int(input("                                                    How Many Infos do you want to Add?: "))  
+        add_in= int(input("                                                  \033[32m \033[01m How Many Infos do you want to Add?:\33[0m "))  
         print('\n \n')
-        u_name= input("   Kindly input your Full Name: ").title()
+        u_name= input("   \033[01mKindly input your Full Name:\33[0m ").title()
         print("")
-        u_age=str(input("   Kindly input your Age: "))
+        u_age=str(input("   \033[01mKindly input your Age:\33[0m "))
         print()
-        u_gender= str(input("   Kindly input your Gender (F/M): ")).title()
+        u_gender= str(input("   \033[01mKindly input your Gender (F/M):\33[0m ")).title()
         print()
-        u_status=input("   Kindly input your Status: ").title()
+        u_status=input("  \033[01m Kindly input your Status:\33[0m ").title()
         print()
-        u_num = str(input("   Kindly input your Phone Number:")).title()
+        u_num = str(input("   \033[01mKindly input your Phone Number:\33[0m ")).title()
         print()
-        u_add = input("   Kindly input your Address: ").title()
+        u_add = input("   \033[01mKindly input your Address:\33[0m ").title()
         print()
-        u_occu = (input("   Kindly input your Occupation:" )).title()
+        u_occu = (input("   \033[01mKindly input your Occupation:\33[0m " )).title()
         print ('\n')
-        print ("                                                       Your Information has been saved!")
+        print("=======================================================================================================================================================================")
+        print('\n')
+        print ("                                                       \033[01mYour Information has been saved!\33[0m ")
         print('\n')
         print("=======================================================================================================================================================================")
         print ('\n \n')
@@ -49,25 +49,26 @@ while True:
         info[u_name]= [u_age,  u_gender, u_status, u_num, u_add, u_occu]
         new_info = info
         
+        
              
 # Option 2
     elif user_in == 2:
-        print("                                                    Information you want to search?: ")  
+        print("                                                    \033[32m \033[01m Information you want to search?:\33[0m ")  
         search= input().title()
         resp=info.get(search)
-        print("                                                      Information of" , search)
         for info1 in resp, new_info:
-            print(str("Age:" ) + info1[0])
             print()
-            print(str("Gender: ") + info1[1])
+            print(str("\033[01m\033[92mAge:\33[0m" ) + info1[0])
             print()
-            print(str("Status: ") + info1[2])
+            print(str("\033[01m\033[92mGender:\33[0m ") + info1[1])
             print()
-            print(str("Phone Number: ") + info1[3])
+            print(str("\033[01m\033[92mStatus:\33[0m ") + info1[2])
             print()
-            print(str("Address: ") + info1[4])
+            print(str("\033[01m\033[92mPhone Number:\33[0m ") + info1[3])
             print()
-            print(str("Occupation: ") + info1[5])
+            print(str("\033[01m\033[92mAddress: \33[0m") + info1[4])
+            print()
+            print(str("\033[01m\033[92mOccupation:\33[0m ") + info1[5])
             print
             break
         
@@ -79,7 +80,7 @@ while True:
         print("Have a Great Day!")
     
     else:
-        print("You must choose between 1-3 only")
+        print("\033[31m                                                         You must choose between 1-3 only\33[0m")
         
         
 
