e99e5ff9988ec8514d8f68edbd6294a4452ed1ce
Kristine <chankristineagcay@gmail.com>
2022-11-12 11:59:42 +0800

    implemented user input

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,22 @@
 # Write a python program for contact tracing:
 # Display a menu
-print("")
+print()
 print("                                      ===================== CONTACT TRACING MENU ========================")
 print()
 print("                                                             [1] Add an Item")
 print("                                                             [2] Search an Item")
 print("                                                             [3] Exit Program")
-print("")
+print()
 print("                                      ===================================================================")
+
+
+# Ask User for input 
+print()
+print()
+user_in = int(input("Annyeonghaseyo, Dear User! What do you want to do?  "))
+
 # Option 1
+
 # Option 2
 # Option 3
 
